0fb01fa886b02088e7a2a5e2f8c3b4378a5b2d02
Zaynah Anjum <zaynahanjum789@gmail.com>
2025-12-03 18:48:50 +0530

    added backend code

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,26 +1,200 @@
 #include "crow.h"
+#include <vector>
+#include <string>
+
+//Song STRUCT
+
+struct Song {
+    std::string title;
+    std::string singer;
+    std::vector<std::string> lyrics;
+};
+
+
+//SongService CLASS
+class SongService {
+public:
+    std::vector<Song> getAllSongs() {
+        return {
+            {
+                "Count on Me",
+                "Bruno Mars",
+                {
+                   "Oh-oh-oh",
+                    "If you ever find yourself stuck in the middle of the sea",
+                    "I'll sail the world to find you",
+                    "If you ever find yourself lost in the dark and you can't see",
+                    "I'll be the light to guide you",
+                    "We find out what we're made of",
+                    "When we are called to help our friends in need",
+                    "You can count on me like one, two, three, I'll be there",                        "And I know when I need it",
+                    "I can count on you like four, three, two and you'll be there",
+                    "'Cause that's what friends are supposed to do, oh, yeah",
+                    "Ooh-ooh-ooh, ooh-ooh-ooh",
+                    "Yeah, yeah",
+                    "If you're tossin' and you're turnin' and you just can't fall asleep",
+                    "I'll sing a song beside you",
+                    "And if you ever forget how much you really mean to me",
+                    "Every day, I will remind you, oh",
+                    "We find out what we're made of",
+                    "When we are called to help our friends in need"
+                }
+            },
+            {
+                "Photograph",
+                "Ed Sheeran",
+                {
+                    "Lovin' can hurt",
+                    "Lovin' can hurt sometimes",
+                    "But it's the only thing that I know",
+                    "And when it gets hard",
+                    "You know it can get hard sometimes",
+                    "It is the only thing that makes us feel alive",
+                    "We keep this love in a photograph",
+                    "We made these memories for ourselves",
+                    "Where our eyes are never closin'",
+                    "Hearts are never broken",
+                    "And time's forever frozen still",
+                    "So you can keep me",
+                    "Inside the pocket of your ripped jeans",
+                    "Holdin' me closer 'til our eyes meet",
+                    "You won't ever be alone",
+                    "Wait for me to come home",
+                    "Lovin' can heal",
+                    "Lovin' can mend your soul",
+                    "And it's the only thing that I know, know",
+                    "I swear it will get easier",
+                    "Remember that with every piece of ya",
+                    "Mm, and it's the only thing we take with us when we die",
+                    "Mm, we keep this love in this photograph",
+                    "We made these memories for ourselves",
+                    "Where our eyes are never closin'",
+                    "Hearts were never broken",
+                    "And time's forever frozen still"
+                }
+            },
+            {
+                "Raabta",
+                "Arijit Singh",
+                {
+                    "Kehte hain:",
+                    "Khuda ne iss jahaan mein",
+                    "Sabhi ke liye kisi na kisi ko hai banaaya",
+                    "har kisi ke liye",
+                    "Tera milna hai uss rab ka ishaara",
+                    "Maano mujhko banaya tere jaise hi kisi ke liye",
+                    "Kuch toh hai tujh se raabta",
+                    "Kuch toh hai tujh se raabta",
+                    "Kaise hum jaane, hume kya pata",
+                    "Kuch toh hai tujh se raabta",
+                    "Tu humsafar hai",
+                    "Phir kya fikar hai",
+                    "Jeene ki wajah hi yehi hai",
+                    "Marna issi ke liye",
+                    "Kehte hain:",
+                    "Khuda ne iss jahaan mein",
+                    "Sabhi ke liye kisi na kisi ko hai banaaya",
+                    "har kisi ke liye…"
+                }
+            },
+            {
+                "A Thousand Years",
+                "Christina Perri",
+                {
+                    "Heart beats fast",
+                    "Colors and promises",
+                    "How to be brave",
+                    "How can I love when I'm afraid to fall",
+                    "But watching you stand alone",
+                    "All of my doubt suddenly goes away somehow",
+                    "One step closer",
+                    "I have died every day waiting for you",
+                    "Darling, don't be afraid",
+                    "I have loved you for a thousand years",
+                    "I'll love you for a thousand more",
+                    "Time stands still",
+                    "Beauty in all she is",
+                    "I will be brave",
+                    "I will not let anything take away",
+                    "What's standing in front of me",
+                    "Every breath, every hour has come to this",
+                    "One step closer",
+                    "I have died every day waiting for you",
+                    "Darling, don't be afraid",
+                    "I have loved you for a thousand years",
+                    "I'll love you for a thousand more"
+                }
+            },
+            {
+                "Dil Diyaa Gallan",
+                "Atif Aslam",
+                {
+                    "Kachchi doriyon doriyon doriyon se",
+                    "Mainu tu baandh le",
+                    "Pakki yaariyon yaariyon yaariyon mein",
+                    "Aunde na faasle",
+                    "Eh narazgi kaagzi saari teri",
+                    "Mere sohneya sun lai meri",
+                    "Dil diyan gallan",
+                    "Karange naal naal beh ke",
+                    "Akh naal akh nu mila ke",
+                    "Dil diyan gallan haye",
+                    "Karange roz roz beh ke",
+                    "Sachiyan mohabbtan nibha ke",
+                    "Sataye mainu kyun",
+                    "Dikhaye mainu kyun",
+                    "Aivein jhoothi moothi",
+                    "Russ ke russa ke",
+                    "Dil diyan gallan",
+                    "Karange naal naal beh ke",
+                    "Akh naal akh nu mila ke"
+                }
+            }
+        };
+    }
+};
+
+
+//SongController CLASS
+
+class SongController {
+private:
+    SongService service;
+
+public:
+    void registerRoutes(crow::SimpleApp& app) {
+
+        // Route: /songs  (returns all 5 songs)
+        CROW_ROUTE(app, "/songs")
+        ([this]() {
+            auto songs = service.getAllSongs();
+
+            crow::json::wvalue result;
+            result["songs"] = crow::json::wvalue::list(songs.size());
+
+            for (size_t i = 0; i < songs.size(); i++) {
+                result["songs"][i]["title"] = songs[i].title;
+                result["songs"][i]["singer"] = songs[i].singer;
+
+                result["songs"][i]["lyrics"] =
+                    crow::json::wvalue::list(songs[i].lyrics.size());
+
+                for (size_t j = 0; j < songs[i].lyrics.size(); j++) {
+                    result["songs"][i]["lyrics"][j] = songs[i].lyrics[j];
+                }
+            }
+
+            return result;
+        });
+    }
+};
+
 
 int main() {
-    crow::App<crow::CORSHandler> app;
+    crow::SimpleApp app;
 
-    // Configure CORS
-    auto& cors = app.get_middleware<crow::CORSHandler>();
-    cors.global()
-        .origin("*")
-        .methods("GET"_method, "POST"_method)
-        .allow_credentials();
-
-    // Route: /hello?name=Zaynah
-    CROW_ROUTE(app, "/hello")
-    ([](const crow::request& req){
-        const char* name = req.url_params.get("name");
-        if (!name) name = "World";
-
-        crow::json::wvalue response;
-        response["message"] = std::string("Hello from Crow, ") + name;
-
-        return response;
-    });
+    SongController sc;
+    sc.registerRoutes(app);
 
     app.port(8080).multithreaded().run();
 }
